04231cb1de84d7efccad882b55caaad817c9032d
TimCauss <t.caussignac@gmail.com>
2023-05-30 15:22:45 +0200

    feat: Adding header/nav & footer.php adding title img and header img

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+    <div class="header-img-ctn">
+        <img src="./img/header_img.png" alt="">
+    </div>
+    <div class="title-container">
+        <img src="./img/Title_header.png" alt="Image du titre de la page stipulant POKELISTE">
+    </div>
+</header>
--- index.php
+++ index.php
@@ -6,8 +6,17 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/style-main.css">
-    <title>Pokedex</title>
+    <title>Pokeliste</title>
 </head>
+
+
+<!-- /** includes Header and Nav */ -->
+<?php
+
+include_once "./header.php";
+include_once "./nav.php";
+
+?>
 
 <body>
 
