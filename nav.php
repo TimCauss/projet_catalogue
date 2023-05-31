ad03c9c5ac00529dd84ec9e01c28b0b03630dae5
TimCauss <t.caussignac@gmail.com>
2023-05-31 10:49:07 +0200

    ✨ feat(NAV):
    Nav HTML/CSS is created

---
+++ nav.php
@@ -0,0 +1,18 @@
+<nav class="main-nav">
+    <ul>
+        <li>
+            <div class="nav-item">
+                Accueil
+            </div>
+        </li>
+        <li>
+            <div class="nav-item">Types</div>
+        </li>
+        <li>
+            <div class="nav-item">Profile</div>
+        </li>
+        <li>
+            <div class="nav-item">Admin</div>
+        </li>
+    </ul>
+</nav>
