9c3710c370a3d62e855390c10cee13099bf91991
TimCauss <t.caussignac@gmail.com>
2023-06-01 20:17:59 +0200

    style: adding favicon

--- creer.php
+++ creer.php
@@ -47,6 +47,7 @@ if ($_POST && !empty($_POST['nom']) && !empty($_POST['numero'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
     <!-- Google Fonts -->
--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="./img/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="./CSS/style-main.css">
     <link rel="stylesheet" href="./CSS/flickity.css">
     <title>Pokeliste - Home</title>
