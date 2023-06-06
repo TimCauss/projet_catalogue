f1d2c7f030b472376c047de0b1a731c27da01b0d
TimCauss <t.caussignac@gmail.com>
2023-06-06 09:16:49 +0200

    refactor: change signup page text

--- login.php
+++ login.php
@@ -31,20 +31,20 @@
             backdrop-filter: blur(30px);
             ">
                         <div class="card-body p-5 shadow-5 text-center">
-                            <h2 class="fw-bold mb-5">Sign up now</h2>
+                            <h2 class="fw-bold mb-5">Créer un compte</h2>
                             <form>
                                 <!-- 2 column grid layout with text inputs for the first and last names -->
                                 <div class="row">
                                     <div class="col-md-6 mb-4">
                                         <div class="form-outline">
                                             <input type="text" id="form3Example1" class="form-control" />
-                                            <label class="form-label" for="form3Example1">First name</label>
+                                            <label class="form-label" for="form3Example1">Prénom</label>
                                         </div>
                                     </div>
                                     <div class="col-md-6 mb-4">
                                         <div class="form-outline">
                                             <input type="text" id="form3Example2" class="form-control" />
-                                            <label class="form-label" for="form3Example2">Last name</label>
+                                            <label class="form-label" for="form3Example2">Nom</label>
                                         </div>
                                     </div>
                                 </div>
@@ -52,19 +52,19 @@
                                 <!-- Email input -->
                                 <div class="form-outline mb-4">
                                     <input type="email" id="form3Example3" class="form-control" />
-                                    <label class="form-label" for="form3Example3">Email address</label>
+                                    <label class="form-label" for="form3Example3">Adresse Email</label>
                                 </div>
 
                                 <!-- Password input -->
                                 <div class="form-outline mb-4">
                                     <input type="password" id="form3Example4" class="form-control" />
-                                    <label class="form-label" for="form3Example4">Password</label>
+                                    <label class="form-label" for="form3Example4">Mot de passe</label>
                                 </div>
 
 
                                 <!-- Submit button -->
                                 <button type="submit" class="btn btn-primary btn-block mb-4">
-                                    Sign up
+                                    S'enregistrer'
                                 </button>
                             </form>
                         </div>
