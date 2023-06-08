2f476f869ba0363e003fab35fdb50d86f8dc6313
TimCauss <t.caussignac@gmail.com>
2023-06-09 00:00:50 +0200

    style: change max-width of main container to 1440px change type tag placement

--- JS/poke-grid.js
+++ JS/poke-grid.js
@@ -50,43 +50,43 @@ function verifTagType(type) {
   } else if (type.innerHTML == "VOL") {
     type.style.background = "#82BAEF";
   } else {
-    type.style.background = "#ffffff";
+    type.style.display = "none";
   }
 }
 
 function VerifType(elem, key) {
   if (elem.getAttribute("data-p_type") == "plante") {
-    circle[key].style.background = "#68D03780";
+    circle[key].style.background = "#68D03790";
   } else if (elem.getAttribute("data-p_type") == "feu") {
-    circle[key].style.background = "#E3350D80";
+    circle[key].style.background = "#E3350D90";
   } else if (elem.getAttribute("data-p_type") == "eau") {
-    circle[key].style.background = "#1E90FF80";
+    circle[key].style.background = "#1E90FF90";
   } else if (elem.getAttribute("data-p_type") == "glace") {
-    circle[key].style.background = "#1DFFFF80";
+    circle[key].style.background = "#1DFFFF90";
   } else if (elem.getAttribute("data-p_type") == "normal") {
-    circle[key].style.background = "#CD853F80";
+    circle[key].style.background = "#CD853F90";
   } else if (elem.getAttribute("data-p_type") == "electrik") {
-    circle[key].style.background = "#FFDB1D80";
+    circle[key].style.background = "#FFDB1D90";
   } else if (elem.getAttribute("data-p_type") == "poison") {
-    circle[key].style.background = "#80008080";
+    circle[key].style.background = "#80008090";
   } else if (elem.getAttribute("data-p_type") == "insecte") {
-    circle[key].style.background = "#729f3f80";
+    circle[key].style.background = "#729f3f90";
   } else if (elem.getAttribute("data-p_type") == "psy") {
-    circle[key].style.background = "#EE82EE80";
+    circle[key].style.background = "#EE82EE90";
   } else if (elem.getAttribute("data-p_type") == "combat") {
-    circle[key].style.background = "#8B000080";
+    circle[key].style.background = "#8B000090";
   } else if (elem.getAttribute("data-p_type") == "acier") {
-    circle[key].style.background = "#c0c0c080";
+    circle[key].style.background = "#c0c0c090";
   } else if (elem.getAttribute("data-p_type") == "tenebres") {
-    circle[key].style.background = "#3d3d3d80";
+    circle[key].style.background = "#3d3d3d90";
   } else if (elem.getAttribute("data-p_type") == "spectre") {
-    circle[key].style.background = "#6E3C6E80";
+    circle[key].style.background = "#6E3C6E90";
   } else if (elem.getAttribute("data-p_type") == "sol") {
-    circle[key].style.background = "#B8860B80";
+    circle[key].style.background = "#B8860B90";
   } else if (elem.getAttribute("data-p_type") == "roche") {
-    circle[key].style.background = "#80808080";
+    circle[key].style.background = "#80808090";
   } else if (elem.getAttribute("data-p_type") == "vol") {
-    circle[key].style.background = "#82BAEF80";
+    circle[key].style.background = "#82BAEF90";
   } else {
     circle.style.background = "#000000";
   }
