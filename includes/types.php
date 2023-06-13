a2f684859adc4691313681348cbe28f8a0b2d814
ArtChard <arthur.chardonnereaupinfamail.com>
2023-06-13 13:44:57 +0200

    13/06

--- includes/types.php
+++ includes/types.php
@@ -1,40 +1,74 @@
 <section id="types" class="section-types">
     <div class="type-card card-feu">
-        <img id="feu" src="./img/icons/fire.svg">
-        Feu
+        <img class="feu svg" src="./img/icons/fire.svg">
+        <span id="feu">Feu</span>
     </div>
     <div class="type-card card-plante">
-        Plante
+    <img class="plante svg" src="./img/icons/grass.svg">
+    <span id="Plante">Plante</span>
     </div>
     <div class="type-card card-Eau">
-        Eau
+    <img class="eau svg" src="./img/icons/water.svg">
+    <span id="Eau">Eau</span>
     </div>
     <div class="type-card card-Glace">
-        Glace
+    <img class="glace svg" src="./img/icons/ice.svg">
+    <span id="Glace">Glace</span>
     </div>
     <div class="type-card card-Normal">
-        Normal
+    <img class="normal svg" src="./img/icons/normal.svg">
+    <span id="Normal">Normal</span>
     </div>
     <div class="type-card card-Electrik">
-        Electrik
+    <img class="electrik svg" src="./img/icons/electric.svg">
+    <span id="Electrik">Electrik</span>
     </div>
     <div class="type-card card-Poison">
-        Poison
+    <img class="poison svg" src="./img/icons/poison.svg">
+    <span id="Poison">Poison</span>
     </div>
     <div class="type-card card-Insecte">
-        Insecte
+    <img class="insecte svg" src="./img/icons/bug.svg">
+    <span id="Insecte">Insecte</span>
     </div>
     <div class="type-card card-Psy">
-         
-        Psy
+    <img class="psy svg" src="./img/icons/psychic.svg">
+    <span id="Psy">Psy</span>
     </div>
     <div class="type-card card-Combat">
-        Combat
+    <img class="combat svg" src="./img/icons/fighting.svg">
+    <span id="Combat">Combat</span>
     </div>
     <div class="type-card card-Acier">
-        Acier
+    <img class="acier svg" src="./img/icons/steel.svg">
+    <span id="Acier">Acier</span>
     </div>
     <div class="type-card card-Tenebres">
-        Ténèbres
+    <img class="tenebres svg" src="./img/icons/dark.svg">
+    <span id="Tenebres">Ténèbres</span>
+    </div>
+    <div class="type-card card-Roche">
+    <img class="roche svg" src="./img/icons/rock.svg">
+    <span id="Roche">Roche</span>
+    </div>
+    <div class="type-card card-Sol">
+    <img class="sol svg" src="./img/icons/ground.svg">
+    <span id="Sol">Sol</span>
+    </div>
+    <div class="type-card card-Spectre">
+    <img class="spectre svg" src="./img/icons/ghost.svg">
+    <span id="Spectre">Spectre</span>
+    </div>
+    <div class="type-card card-Vol">
+    <img class="vol svg" src="./img/icons/flying.svg">
+    <span id="Vol">Vol</span>
+    </div>
+    <div class="type-card card-Dragon">
+    <img class="dragon svg" src="./img/icons/dragon.svg">
+    <span id="Dragon">Dragon</span>
+    </div>
+    <div class="type-card card-Fee">
+    <img class="fee svg" src="./img/icons/fairy.svg">
+    <span id="Fee">Fée</span>
     </div>
 </section>
