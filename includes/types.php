3435dad8b446d811084543fafa02da1771cebc4d
TimCauss <t.caussignac@gmail.com>
2023-06-16 09:53:43 +0200

    feat: link card type working

--- includes/types.php
+++ includes/types.php
@@ -4,71 +4,73 @@
         <span id="feu">Feu</span>
     </div>
     <div class="type-card card-plante">
-    <img class="plante svg" src="./img/icons/grass.svg">
-    <span id="Plante">Plante</span>
+        <img class="plante svg" src="./img/icons/grass.svg">
+        <span id="Plante">Plante</span>
     </div>
     <div class="type-card card-Eau">
-    <img class="eau svg" src="./img/icons/water.svg">
-    <span id="Eau">Eau</span>
+        <img class="eau svg" src="./img/icons/water.svg">
+        <span id="Eau">Eau</span>
     </div>
     <div class="type-card card-Glace">
-    <img class="glace svg" src="./img/icons/ice.svg">
-    <span id="Glace">Glace</span>
+        <img class="glace svg" src="./img/icons/ice.svg">
+        <span id="Glace">Glace</span>
     </div>
     <div class="type-card card-Normal">
-    <img class="normal svg" src="./img/icons/normal.svg">
-    <span id="Normal">Normal</span>
+        <img class="normal svg" src="./img/icons/normal.svg">
+        <span id="Normal">Normal</span>
     </div>
     <div class="type-card card-Electrik">
-    <img class="electrik svg" src="./img/icons/electric.svg">
-    <span id="Electrik">Electrik</span>
+        <img class="electrik svg" src="./img/icons/electric.svg">
+        <span id="Electrik">Electrik</span>
     </div>
     <div class="type-card card-Poison">
-    <img class="poison svg" src="./img/icons/poison.svg">
-    <span id="Poison">Poison</span>
+        <img class="poison svg" src="./img/icons/poison.svg">
+        <span id="Poison">Poison</span>
     </div>
     <div class="type-card card-Insecte">
-    <img class="insecte svg" src="./img/icons/bug.svg">
-    <span id="Insecte">Insecte</span>
+        <img class="insecte svg" src="./img/icons/bug.svg">
+        <span id="Insecte">Insecte</span>
     </div>
     <div class="type-card card-Psy">
-    <img class="psy svg" src="./img/icons/psychic.svg">
-    <span id="Psy">Psy</span>
+        <img class="psy svg" src="./img/icons/psychic.svg">
+        <span id="Psy">Psy</span>
     </div>
     <div class="type-card card-Combat">
-    <img class="combat svg" src="./img/icons/fighting.svg">
-    <span id="Combat">Combat</span>
+        <img class="combat svg" src="./img/icons/fighting.svg">
+        <span id="Combat">Combat</span>
     </div>
     <div class="type-card card-Acier">
-    <img class="acier svg" src="./img/icons/steel.svg">
-    <span id="Acier">Acier</span>
+        <img class="acier svg" src="./img/icons/steel.svg">
+        <span id="Acier">Acier</span>
     </div>
     <div class="type-card card-Tenebres">
-    <img class="tenebres svg" src="./img/icons/dark.svg">
-    <span id="Tenebres">Ténèbres</span>
+        <img class="tenebres svg" src="./img/icons/dark.svg">
+        <span id="Tenebres">Ténèbres</span>
     </div>
     <div class="type-card card-Roche">
-    <img class="roche svg" src="./img/icons/rock.svg">
-    <span id="Roche">Roche</span>
+        <img class="roche svg" src="./img/icons/rock.svg">
+        <span id="Roche">Roche</span>
     </div>
     <div class="type-card card-Sol">
-    <img class="sol svg" src="./img/icons/ground.svg">
-    <span id="Sol">Sol</span>
+        <img class="sol svg" src="./img/icons/ground.svg">
+        <span id="Sol">Sol</span>
     </div>
     <div class="type-card card-Spectre">
-    <img class="spectre svg" src="./img/icons/ghost.svg">
-    <span id="Spectre">Spectre</span>
+        <img class="spectre svg" src="./img/icons/ghost.svg">
+        <span id="Spectre">Spectre</span>
     </div>
     <div class="type-card card-Vol">
-    <img class="vol svg" src="./img/icons/flying.svg">
-    <span id="Vol">Vol</span>
+        <img class="vol svg" src="./img/icons/flying.svg">
+        <span id="Vol">Vol</span>
     </div>
     <div class="type-card card-Dragon">
-    <img class="dragon svg" src="./img/icons/dragon.svg">
-    <span id="Dragon">Dragon</span>
+        <img class="dragon svg" src="./img/icons/dragon.svg">
+        <span id="Dragon">Dragon</span>
     </div>
     <div class="type-card card-Fee">
-    <img class="fee svg" src="./img/icons/fairy.svg">
-    <span id="Fee">Fée</span>
+        <img class="fee svg" src="./img/icons/fairy.svg">
+        <span id="Fee">Fée</span>
     </div>
 </section>
+
+<script src="./JS/cards.js"></script>
