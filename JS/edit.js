a8f9f28c7c196c58ce258c8ff5c8e70f4e1c1c33
TimCauss <t.caussignac@gmail.com>
2023-06-19 08:08:38 +0200

    (edit.js)Removing useless clg

--- JS/edit.js
+++ JS/edit.js
@@ -1,7 +1,6 @@
 function readURL(input) {
   if (input.files && input.files[0]) {
     let reader = new FileReader();
-    console.log(reader);
     reader.onload = function (e) {
       document.getElementById("pokemon").setAttribute("src", e.target.result);
     };
